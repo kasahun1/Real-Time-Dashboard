1ccd86edbc278c29e45fd53b2aeb3d0603f681d7
kasahun1 <kasuabebe12@gmail.com>
2024-05-23 15:49:55 +0300

    all setuo and home page started

---
+++ src/components/home/index.ts
@@ -0,0 +1,4 @@
+import DealsChart from "./deals-charts";
+import UpcomingEvents from "./upcoming-events";
+
+export {UpcomingEvents, DealsChart}
---
+++ src/components/home/upcoming-events.tsx
@@ -0,0 +1,39 @@
+import { CalendarOutlined } from "@ant-design/icons";
+import { Card, List } from "antd";
+import React, { useState } from "react";
+import { Text } from "../text";
+import { ListButton } from "@refinedev/antd";
+
+const UpcomingEvents = () => {
+    const [isLoading, setIsLoading] = useState(true)
+
+  return (
+    <Card 
+    style={{height: "100%"}} 
+    headStyle={{padding: "8px 16px"}} 
+    bodyStyle={{padding: "0 1rem"}}
+    title={
+        <div style={{display:"flex", alignItems: "center", gap: "8px"}}>
+          <CalendarOutlined/>
+          <Text size="sm" style={{marginLeft: "0.7rem"}}>
+            Upcoming Events
+          </Text>
+        </div>
+    }
+    >
+     {
+        isLoading ? (
+            <List>
+
+            </List>
+        ) : (
+            <List>
+                
+            </List>
+        )
+     }
+    </Card>
+  )
+};
+
+export default UpcomingEvents;
