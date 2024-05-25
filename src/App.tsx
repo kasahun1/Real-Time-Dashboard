946692e3ea17dc1d977105837f9e4dbe14c2f532
kasahun1 <kasuabebe12@gmail.com>
2024-05-25 22:03:15 +0300

    company started

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import CompanyList from "./pages/company/list";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 function App() {
   return (
@@ -52,7 +54,11 @@ function App() {
                   </Layout>
                   </Authenticated> } >
                   <Route index element={<Home />} />
-                  <Route path="/companies" element={<CompanyList />} />
+                  <Route path="/companies" >
+                    <Route index element={<CompanyList />}/>
+                    <Route path="new" element={<Create />}/>
+                    <Route path="edit/:id" element={<Edit />}/>
+                  </Route>
                   </Route> 
                 </Routes>
                 <RefineKbar />
